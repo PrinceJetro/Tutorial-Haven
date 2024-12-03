d052a53ea9cf39dec5a4935e6d1585d3b885bda7
PrinceJetro <adegbuyijephthah@gmail.com>
2024-12-04 00:35:32 +0100

    included the laTex stuf

--- webapp/cbt.py
+++ webapp/cbt.py
@@ -1,183 +1,146 @@
 questions = [
-    {
-        'course_id': 1,
-        'question_text': "Low annual rainfall, sparse vegetation, high diurnal temperatures and cold nights are characteristic features of the",
-        'option_a': "tropical rainforest",
-        'option_b': "desert",
-        'option_c': "montane forest",
-        'option_d': "guinea savanna",
-        'correct_option': "B",
-    },
-    {
-        'course_id': 1,
-        'question_text': "The activity of an organism which affects the survival of another organism in the same habitat constitutes",
-        'option_a': "an edaphic factor",
-        'option_b': "an abiotic factor",
-        'option_c': "a biotic factor",
-        'option_d': "a physiographic factor",
-        'correct_option': "C",
-    },
-    {
-        'course_id': 1,
-        'question_text': "The average number of individuals of a species per unit area of the habitat is the",
-        'option_a': "population density",
-        'option_b': "population frequency",
-        'option_c': "population size",
-        'option_d': "population distribution",
-        'correct_option': "A",
-    },
-    {
-        'course_id': 1,
-        'question_text': "The vector for yellow fever is",
-        'option_a': "Aedes mosquito",
-        'option_b': "Anopheles mosquito",
-        'option_c': "tsetse fly",
-        'option_d': "blackfly",
-        'correct_option': "A",
-    },
-    {
-        'course_id': 1,
-        'question_text': "The loss of soil through erosion can be reduced by",
-        'option_a': "watering",
-        'option_b': "crop rotation",
-        'option_c': "manuring",
-        'option_d': "irrigation",
-        'correct_option': "B",
-    },
-    {
-        'course_id': 1,
-        'question_text': "The protozoan plasmodium falciparum is transmitted by",
-        'option_a': "female Anopheles mosquitoes",
-        'option_b': "female Aedes mosquitoes",
-        'option_c': "female Culex mosquitoes",
-        'option_d': "Female blackfly",
-        'correct_option': "A",
-    },
-    {
-        'course_id': 1,
-        'question_text': "Which of the following is most advanced in the evolutionary trend of animals?",
-        'option_a': "Liver fluke",
-        'option_b': "Earthworm",
-        'option_c': "Snail",
-        'option_d': "Cockroach",
-        'correct_option': "B",
-    },
-    {
-        'course_id': 1,
-        'question_text': "Which of the following is the lowest category of classification?",
-        'option_a': "Class",
-        'option_b': "Species",
-        'option_c': "Family",
-        'option_d': "Order",
-        'correct_option': "B",
-    },
-    {
-        'course_id': 1,
-        'question_text': "Plants that show secondary growth are usually found among the",
-        'option_a': "thallophytes",
-        'option_b': "pteridophytes",
-        'option_c': "monocotyledons",
-        'option_d': "dicotyledons",
-        'correct_option': "D",
-    },
-    {
-        'course_id': 1,
-        'question_text': "The fungi are distinct group of eukaryotes mainly because they have",
-        'option_a': "spores",
-        'option_b': "no chlorophyll",
-        'option_c': "many fruiting bodies",
-        'option_d': "sexual and sexual reproduction",
-        'correct_option': "B",
-    },
-    {
-        'course_id': 1,
-        'question_text': "An arthropod that is destructive at early stage of its life cycle is",
-        'option_a': "butterfly",
-        'option_b': "mosquito",
-        'option_c': "bee",
-        'option_d': "millipede",
-        'correct_option': "A",
-    },
-    {
-        'course_id': 1,
-        'question_text': "An animal body that can be cut along its axis in any plane to give two identical parts is said to be",
-        'option_a': "radially symmetrical",
-        'option_b': "bilaterally symmetrical",
-        'option_c': "asymmetrical",
-        'option_d': "symmetrical",
-        'correct_option': "A",
-    },
-    {
-        'course_id': 1,
-        'question_text': "Which of the following possesses mammary gland?",
-        'option_a': "Dogfish",
-        'option_b': "whale",
-        'option_c': "shark",
-        'option_d': "catfish",
-        'correct_option': "B",
-    },
-    {
-        'course_id': 1,
-        'question_text': "The feature that links birds to reptiles in evolution is the possession of",
-        'option_a': "feathers",
-        'option_b': "break",
-        'option_c': "skeleton",
-        'option_d': "scales",
-        'correct_option': "D",
-    },
-    {
-        'course_id': 1,
-        'question_text': "The process in which complex substances are broken down into simpler ones is referred to as",
-        'option_a': "anabolism",
-        'option_b': "catabolism",
-        'option_c': "metabolism",
-        'option_d': "tropism",
-        'correct_option': "B",
-    },
-    {
-        'course_id': 1,
-        'question_text': "The organ which is sensitive to light in Euglena is the",
-        'option_a': "gullet",
-        'option_b': "flagellum",
-        'option_c': "chloroplast",
-        'option_d': "eyespot",
-        'correct_option': "D",
-    },
-    {
-        'course_id': 1,
-        'question_text': "The organelles present in cells that are actively respiring and photosynthesizing are",
-        'option_a': "lysosomes and ribosomes",
-        'option_b': "Golgi apparatus and endoplasmic reticulum",
-        'option_c': "nucleus and centrioles",
-        'option_d': "mitochondria and chloroplast",
-        'correct_option': "D",
-    },
-    {
-        'course_id': 1,
-        'question_text': "Taenia solium can be found in",
-        'option_a': "cow",
-        'option_b': "goat",
-        'option_c': "dog",
-        'option_d': "pig",
-        'correct_option': "D",
-    },
-    {
-        'course_id': 1,
-        'question_text': "The structure labelled II is the",
-        'option_a': "spermathecal pore",
-        'option_b': "cocoon",
-        'option_c': "clitellum",
-        'option_d': "chaetae",
-        'correct_option': "C",
-    },
+  {
+    "course_id": "3",
+    "question_text": "A group of market women sell at least one of yam, plantain and maize. 12 of them sell maize, 10 sell yam and 14 sell plantain. 5 sell plantain and maize, 4 sell yam and maize, 2 sell yam and plantain only while 3 sell all the three items. How many women are in the group?",
+    "option_a": "25",
+    "option_b": "19",
+    "option_c": "18",
+    "option_d": "17",
+    "correct_option": "B"
+  },
+  {
+    "course_id": "3",
+    "question_text": "If log 10 to base 8 = X, evaluate log 5 to base 8 in terms of X.",
+    "option_a": "\(\frac{1}{2}X\)",
+    "option_b": "X-\(\frac{1}{4}\)",
+    "option_c": "X-\(\frac{1}{3}\)",
+    "option_d": "X-\(\frac{1}{2}\)",
+    "correct_option": "C"
+  },
+  {
+    "course_id": "3",
+    "question_text": "Find the value of X if \(\frac{\sqrt{2}}{x+\sqrt{2}}=\frac{1}{x-\sqrt{2}}\)",
+    "option_a": "3√2+4",
+    "option_b": "3√2-4",
+    "option_c": "3-2√2",
+    "option_d": "4+2√2",
+    "correct_option": "D"
+  },
+  {
+    "course_id": "3",
+    "question_text": "If \(\frac{(a^2 b^{-3}c)^{\frac{3}{4}}}{a^{-1}b^{4}c^{5}}=a^{p} b^{q} c^{r}\) What is the value of p+2q?",
+    "option_a": "(5/2)",
+    "option_b": "-(5/4)",
+    "option_c": "-(25/4)",
+    "option_d": "-10",
+    "correct_option": "D"
+  },
+  {
+    "course_id": "3",
+    "question_text": "A trader bought 100 oranges at 5 for N1.20, 20 oranges got spoilt and the remaining were sold at 4 for N1.50. Find the percentage gain or loss.",
+    "option_a": "30% gain",
+    "option_b": "25% gain",
+    "option_c": "30% loss",
+    "option_d": "25% loss",
+    "correct_option": "B"
+  },
+  {
+    "course_id": "3",
+    "question_text": "What is the answer when 2434\(_6\) is divided by 42\(_6\)?",
+    "option_a": "236",
+    "option_b": "356",
+    "option_c": "526",
+    "option_d": "556",
+    "correct_option": "A"
+  },
+  {
+    "course_id": "3",
+    "question_text": "If 2\(_9\) x (Y3)\(_9\) = 3\(_5\) x (Y3)\(_5\), find the value of Y.",
+    "option_a": "4",
+    "option_b": "3",
+    "option_c": "2",
+    "option_d": "1",
+    "correct_option": "C"
+  },
+  {
+    "course_id": "3",
+    "question_text": "Simplify \(\sqrt{\frac{(0.0023 \times 750)}{(0.00345 \times 1.25)}}\)",
+    "option_a": "15",
+    "option_b": "20",
+    "option_c": "40",
+    "option_d": "75",
+    "correct_option": "A"
+  },
+  {
+    "course_id": "3",
+    "question_text": "If \(m*n = (\frac{m}{n} - \frac{n}{m})\) for m, n belong to R, evaluate -3*4",
+    "option_a": "\(\frac{-25}{12}\)",
+    "option_b": "\(\frac{-7}{12}\)",
+    "option_c": "\(\frac{7}{12}\)",
+    "option_d": "\(\frac{25}{12}\)",
+    "correct_option": "B"
+  },
+  {
+    "course_id": "3",
+    "question_text": "The sum of two numbers is twice their difference. If the difference of the numbers is P, find the larger of the two numbers",
+    "option_a": "p/2",
+    "option_b": "3p/2",
+    "option_c": "5p/2",
+    "option_d": "3p",
+    "correct_option": "B"
+  },
+  {
+    "course_id": "3",
+    "question_text": "A binary operation * is defined by a*b = ab+a+b for any real number a and b. if the identity element is zero, find the inverse of 2 under this operation.",
+    "option_a": "2/3",
+    "option_b": "1/2",
+    "option_c": "-1/2",
+    "option_d": "-2/3",
+    "correct_option": "D"
+  },
+  {
+    "course_id": "3",
+    "question_text": "Factorize completely \(x^{2} + 2xy + y^{2} + 3x + 3y - 18\).",
+    "option_a": "(x+y+6)(x+y-3)",
+    "option_b": "(x-y-6)(x-y+3)",
+    "option_c": "(x-y+6)(x-y-3)",
+    "option_d": "(x+y-6)(x+y+3)",
+    "correct_option": "A"
+  },
+  {
+    "course_id": "3",
+    "question_text": "Tope bought X oranges at N5.00 each and some mangoes at N4.00 each. if she bought twice as many mangoes as oranges and spent at least N65.00 and at most N130.00, find the range of values of X.",
+    "option_a": "4≤X≤5",
+    "option_b": "5≤X≤8",
+    "option_c": "5≤X≤10",
+    "option_d": "8≤X≤10",
+    "correct_option": "B"
+  },
+  {
+    "course_id": "3",
+    "question_text": "Three consecutive positive integers k, l and m are such that l\(^2\) = 3(k+m). Find the value of m.",
+    "option_a": "4",
+    "option_b": "5",
+    "option_c": "6",
+    "option_d": "7",
+    "correct_option": "C"
+  },
+  {
+    "course_id": "3",
+    "question_text": "Express \(\frac{1}{x^{3}-1}\) in partial fractions",
+    "option_a": "\(\frac{1}{3}(\frac{1}{x - 1} - \frac{(x + 2)}{x^{2} + x + 1})\)",
+    "option_b": "\(\frac{1}{3}(\frac{1}{x - 1} - \frac{x - 2}{x^{2} + x + 1})\)",
+    "option_c": "\(\frac{1}{3}(\frac{1}{x - 1} - \frac{(x - 2)}{x^{2} + x + 1})\)",
+    "option_d": "\(\frac{1}{3}(\frac{1}{x - 1} - \frac{(x - 1)}{x^{2} - x - 1})\)",
+    "correct_option": "A"
+  }
 ]
-
 
 
 
 # questions = [
 #      {
-#         'course_id': 1,
+#         'course_id': 10,
 #         'question_text': "",
 #         'option_a': "",
 #         'option_b': "",
@@ -208,3 +171,5 @@ questions = [
 #         print(f"Course with ID {question_data['course_id']} does not exist.")
 #     except Exception as e:
 #         print(f"An error occurred: {e}")
+
+
